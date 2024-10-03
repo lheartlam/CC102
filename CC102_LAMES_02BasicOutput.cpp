1229fc7153121abeb3f11affb3483ce0f8f8f408
Lovely Heart A. Lames <lovelyheartalames@gmail.com>
2024-10-03 10:02:03 +0800

    Add files via upload

---
+++ CC102_LAMES_02BasicOutput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout <<"Our Lady of Lourdes Foundation" << endl;
+	cout <<"College Computer Studies" << endl;
+	cout <<"Bachelor of Science in Information Technology" << endl;
+	cout <<"Lovely Heart A. Lames" << endl;
+	cout <<"Purok-6, Parang, Jose Panganiban Camarines Norte" << endl;
+	return 0;
+	
+}	
---
+++ CC102_LAMES_03VariableDeclaration.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout <<"19" << endl;
+	cout <<"Thank you!" << endl;
+	
+	return 0;
+	
+}	
