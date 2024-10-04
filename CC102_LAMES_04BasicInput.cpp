5a9426c5fa2ee254dc758e820c406fd1a28cbe66
Lovely Heart A. Lames <lovelyheartalames@gmail.com>
2024-10-04 08:13:46 +0800

    Add files via upload

---
+++ CC102_LAMES_04BasicInput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout <<"Lovely" << endl;
+	cout <<"BSIT-1" << endl;
+	cout <<"OLLCF-DAET,CAMARINES NORTE " << endl;
+	
+	return 0;
+	
+}	
